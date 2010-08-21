defaf0205a3b9be1a5a00bfecf0ba58603786d0b
Marco Peereboom <marco@conformal.com>
2010-08-21 20:57:20 +0000

    More examples

--- examples/testclog.c
+++ examples/testclog.c
@@ -10,25 +10,25 @@ main(int argc, char *argv[])
 	clog_init(1);
 
 	if (clog_set_flags(CLOG_F_FILE | CLOG_F_FUNC | CLOG_F_LINE |
-	    CLOG_F_STDERR | CLOG_F_ENABLED | CLOG_F_DTIME))
+	    CLOG_F_STDERR | CLOG_F_ENABLE | CLOG_F_DTIME))
 		errx(1, "can't set clog flags");
 	clog_set_mask(CLOGTEST_F_LEVEL1 | CLOGTEST_F_LEVEL2);
 
 	CNDBG(CLOGTEST_F_LEVEL3, "not printed moo %s", "meh");
 	CNDBG(CLOGTEST_F_LEVEL1, "printed moo %s", "meh");
-	CDBG("too %s", "coo");
+	CDBG("dbg too %s", "coo");
 
-	if (clog_set_flags(CLOG_F_STDERR | CLOG_F_ENABLED | CLOG_F_DTIME | CLOG_F_DATE))
+	if (clog_set_flags(CLOG_F_STDERR | CLOG_F_ENABLE | CLOG_F_DTIME | CLOG_F_DATE))
 		errx(1, "can't clear clog flags");
 
 	CNDBG(1, "moo %s", "meh");
-	CDBG("too %s", "coo");
+	CDBG("dbg2 too %s", "coo");
 
-	if (clog_set_flags(CLOG_F_STDERR | CLOG_F_ENABLED | CLOG_F_DATE | CLOG_F_SYSLOG))
+	if (clog_set_flags(CLOG_F_STDERR | CLOG_F_ENABLE | CLOG_F_DATE | CLOG_F_SYSLOG | CLOG_F_DBGENABLE))
 		errx(1, "can't clear clog flags");
 
 	CNDBG(1, "moo %s", "meh");
-	CDBG("too %s", "coo");
+	CDBG("dbg 3 too %s", "coo");
 
 	CWARNX("OMG WARNING");
 	CWARN("OMG WARNING with errno");
